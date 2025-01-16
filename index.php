a69d3eba8409288bc868dc9312bfd28ddf41048e
GzCh28 <gchgazent@gmail.com>
2025-01-16 13:57:30 +0700

    index commit

--- index.php
+++ index.php
@@ -8,20 +8,35 @@
 <body>
   <h1>Animals</h1>
   <?php
-    class Animal {
-      public $legs = 4;
-      public $coldBlooded = 'no';
-      public $name;
-      public function __construct($nameInput) {
-        $this->name = $nameInput;
-      }
-    }
+    require_once 'animal.php';
+    require_once 'frog.php';
+    require_once 'ape.php';
+    
 
-    $sheep = new Animal("shaun");
+    $sheep = new Animal(4,"shaun");
 
-    echo $sheep->name . '<br>'; // "shaun"
-    echo $sheep->legs . '<br>'; // 4
-    echo $sheep->coldBlooded . '<br>'; // "no"
+    echo "Name: $sheep->name <br>";
+    echo "Legs: $sheep->legs <br>";
+    echo "Cold Blooded: $sheep->coldBlooded <br><br>";
+
+
+    $sungokong = new Ape("kera sakti");
+    echo "Name: $sungokong->name <br>";
+    echo "Legs: $sungokong->legs <br>";
+    echo "Cold Blooded: $sungokong->coldBlooded <br>";
+    echo "Yell: ";
+    $sungokong->yell(); // "Auooo"
+    echo '<br>';
+    
+
+    $kodok = new Frog("buduk");
+    echo "Name: $kodok->name <br>";
+    echo "Legs: $kodok->legs <br>";
+    echo "Cold Blooded: $kodok->coldBlooded <br>";
+    echo "Jump: ";
+    $kodok->jump(); // "hop hop"
+    echo '<br>';
+    
 
   ?>
 </body>
